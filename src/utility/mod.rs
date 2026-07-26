59aaaaf8c88f60dae45f7d99150b4538750996f5
BDKR-actual <bdkr@protonmail.ch>
2026-07-26 00:46:11 -0400

    Tighten up spacing and formatting

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -15,7 +15,20 @@ pub struct timer
 	}
 
 
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct log_file_data
+	{
+	pub now:			u64,
+	pub new:			u64,
+	pub accum:			u64,
+	pub elapsed:		u64,
 
+	pub file_name:		String,
+	pub file_path:		String,
+	}
 
 
 pub mod utility;
--- src/utility/utility.rs
+++ src/utility/utility.rs
@@ -1,8 +1,20 @@
 
 
-
+extern crate dirs;
 use epoch_timestamp::Epoch;             // Straightforward Unix Epoch as seconds
 use crate::utility::timer;
+// use crate::utility::config_data;
+use std::collections::HashMap;
+
+use std::fs::File;
+use std::fs::exists;
+use std::env;
+use std::
+    {
+    io::{prelude::*, BufReader},
+    path::Path,
+    };
+
 
 impl timer
 	{
@@ -21,12 +33,50 @@ impl timer
 
 		return self.elapsed;
 		}
-
 	}
 
 
+/* This is really a sub-function of read_config. Does as named and returns the file as lines in a vector */
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String>
+    {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+    }
 
 
+pub fn read_config(conf_store: &mut HashMap<String, String>)
+    {
+    /* Let's generate a string rep of our configuration file */
+    let conf_tail                           = "/nvid_fan_controller/config";
+    let dirs_act1                           = dirs::config_dir().expect("Error: Failed to open the home directory!!\n");    // Assumes ~/.config
+    let dirs_act2: String                   = dirs_act1.to_str().unwrap().to_string();                                      // Converts findings above to String
+    let conf_path: String                   = dirs_act2+conf_tail;
+
+    /* Let's now open the file, iterate, and assign pertinent values */
+    let lines_ref   = lines_from_file(conf_path);   // Get each line from the config file as an entry in a vector
+    let lines       = lines_ref.clone();
+    for l in &lines
+        {
+        if(l.contains("//") || l.chars().count()==0) { /* Do nothing */ }   /* Yes, I could do this another way, but this feels explicit. */
+        else
+            {
+            if(l.contains(":"))
+                {
+                let mut b2: Vec<&str>       = l.split(":").collect::<Vec<&str>>();
+                // I know the correct config format should only result in two elements. If more, I'll ignore the third + element(s). 
+                // Parse the string and return it to it's own value 
+                let mut b2_00: String = b2[0].replace("\t", "").trim().to_string();
+                let mut b2_01: String = b2[1].replace("\t", "").trim().to_string();
+
+                // Now push those good strings into the HashMap 
+                conf_store.insert(String::from(b2_00), String::from(b2_01));
+                }
+            }
+        }
+    }
 
 
 
